8634606b6e8d7762aa9065df943e1ca80b2dbe57
Prang, Andreas <andreas.prang@bild.de>
2015-10-18 19:22:04 +0200

    Initial Commit

---
+++ Öffentlich Rechtliche LIVE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Öffentlich Rechtliche LIVE
+//
+//  Created by Prang, Andreas on 18.10.15.
+//  Copyright © 2015 iSolute-Berlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
